545a3786809874a5c60a6ed0baa9adc627116395
1640 Sab-BOT-age <temp>
2023-02-02 21:20:30 -0500

    Not fixed

--- src/main/java/frc/robot/subsystems/drive/commands/JoystickDriveCommand.java
+++ src/main/java/frc/robot/subsystems/drive/commands/JoystickDriveCommand.java
@@ -21,6 +21,8 @@ public class JoystickDriveCommand extends CommandBase {
     DriveSubsystem driveSubsystem;
     Gyro gyro;
     XboxController driverController;
+    
+    Trigger leftTrigger = new Trigger(() -> driverController.getLeftTriggerAxis() > 0.1);
 
     JoystickCleaner joystickCleaner = new JoystickCleaner();
 
@@ -32,7 +34,7 @@ public class JoystickDriveCommand extends CommandBase {
 
     private boolean gyroCorrectionOn = false;
     private double gyroCorrectionHeading;
-    private PIDController gyroCorrectionPID = new PIDController(0.025, 0, 0);
+    private PIDController gyroCorrectionPID = new PIDController(0.5, 0, 0);
 
     public JoystickDriveCommand(DriveSubsystem driveSubsystem, boolean fieldRelative, Gyro gyro, XboxController driveController) {
         this.fieldRelative = fieldRelative;
@@ -40,6 +42,8 @@ public class JoystickDriveCommand extends CommandBase {
         this.gyro = gyro;
         this.driverController = driveController;
         addRequirements(driveSubsystem);
+
+        gyroCorrectionPID.enableContinuousInput(-Math.PI, Math.PI);
     }
 
     @Override
@@ -55,19 +59,19 @@ public class JoystickDriveCommand extends CommandBase {
 
         if (driverController.getBackButtonPressed()) {
             fieldRelative = !fieldRelative;
+            System.out.format("%b\n", fieldRelative);
         }
-
-        Trigger leftTrigger = new Trigger(() -> driverController.getLeftTriggerAxis() > 0.1);
 
         if(leftTrigger.getAsBoolean()) {
             xSpeed = -driverController.getLeftY() * SLOW_LINEAR_SPEED;
             ySpeed = -driverController.getLeftX() * SLOW_LINEAR_SPEED;
             rot = -driverController.getRightX() * SLOW_ROTATIONAL_SPEED;
         } else {
-            xSpeed = -m_xspeedLimiter.calculate(driverController.getLeftY());
-            ySpeed = -m_yspeedLimiter.calculate(driverController.getLeftX());
-            rot = -m_rotLimiter.calculate(driverController.getRightX());
+            xSpeed = -driverController.getLeftY();
+            ySpeed = -driverController.getLeftX();
+            rot = -driverController.getRightX();
         }
+        // System.out.format("%.2f\n", Math.toDegrees(Math.atan2(ySpeed, xSpeed)));
 
         /* Apply linear deadband */
         joystickCleaner.setX(xSpeed);
@@ -82,8 +86,10 @@ public class JoystickDriveCommand extends CommandBase {
         /* Increase rotational sensitivity */
         rot = Math.signum(rot) * Math.pow(Math.abs(rot), 1.0 / 3.0);
 
+        System.out.format("%.2f\n", Math.toDegrees(Math.atan2(ySpeed, xSpeed)));
+
         /* Determine whether to apply gyro correction */
-        if(rot == 0 && Math.abs(gyro.getGyroRoll()) < 2 && Math.abs(gyro.getGyroPitch()) < 2){
+        if(Math.abs(rot) < 0.01 && Math.abs(gyro.getGyroRoll()) < 5 && Math.abs(gyro.getGyroPitch()) < 5){
             if (!gyroCorrectionOn) {
                 gyroCorrectionOn = true;
                 gyroCorrectionHeading = gyro.getGyroAngle().getRadians();
@@ -95,8 +101,12 @@ public class JoystickDriveCommand extends CommandBase {
 
         /* Apply gyro correction */
         if (gyroCorrectionOn) {
-            rot = gyroCorrectionPID.calculate(gyro.getGyroAngle().getRadians(), gyroCorrectionHeading);
+            // rot = gyroCorrectionPID.calculate(gyro.getGyroAngle().getRadians(), gyroCorrectionHeading);
         }
+
+        // System.out.format("%.2f, %.2f, %s\n", Math.toDegrees(gyroCorrectionHeading), rot, Boolean.toString(gyroCorrectionOn));
+        // System.out.format("%.2f, %.2f\n", Math.hypot(xSpeed, ySpeed), rot);
+        // System.out.format("%.2f\n", Math.toDegrees(Math.atan2(ySpeed, xSpeed)));
 
         driveSubsystem.drive(xSpeed, ySpeed, rot, fieldRelative);
     }
