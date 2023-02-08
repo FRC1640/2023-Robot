af366864d07ad5cdfc735f861b79e5b04afbec8e
Nitsuj06 <nitsujwriter@gmail.com>
2023-02-07 19:59:15 -0500

    set arm mode

--- src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -1,11 +1,29 @@
 package frc.robot.subsystems.arm;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase{
+    public enum ArmMode {
+        manual(0),
+        endEffector(1),
+        preset(2);
+
+        public final int value;
+    
+        ArmMode(int value) {
+          this.value = value;
+        }
+    }
+    ArmMode mode;
     @Override
     public void periodic() {
         
         super.periodic();
+    }
+    public void setMode(ArmMode newMode){
+        mode = newMode;
     }
 }
