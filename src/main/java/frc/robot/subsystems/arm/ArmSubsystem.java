245de0441f6410cd4efd92f27d6673a2549b06be
Nitsuj06 <nitsujwriter@gmail.com>
2023-02-07 19:04:45 -0500

    arm subsystem

---
+++ src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase{
+    @Override
+    public void periodic() {
+        
+        super.periodic();
+    }
+}
