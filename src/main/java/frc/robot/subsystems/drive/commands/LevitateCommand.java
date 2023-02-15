c29ea7c301fe181b6514676f5a0813f67e0f140f
Vedansh Nikum <nikum.vedansh@gmail.com>
2023-02-14 19:37:30 -0500

    most of levitate command finished

--- src/main/java/frc/robot/subsystems/drive/commands/LevitateCommand.java
+++ src/main/java/frc/robot/subsystems/drive/commands/LevitateCommand.java
@@ -1,58 +1,53 @@
 package frc.robot.subsystems.drive.commands;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.sensors.DistanceSensor;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
-public class LevitateCommand extends CommandBase{
-    DistanceSensor leftSensor = new DistanceSensor(4);
-    DistanceSensor rightSensor = new DistanceSensor(5);
-    DriveSubsystem driveSubsystem;
-    public LevitateCommand (DriveSubsystem driveSubsystem) {
-        this.driveSubsystem = driveSubsystem;
-    }
-    
-    @Override
-    public void initialize() {}
+public class LevitateCommand extends CommandBase {
+  DigitalInput levitateSwitch = new DigitalInput(0);//TODO: add IDs
+  DistanceSensor leftSensor = new DistanceSensor(4);//TODO: add IDs
+  DistanceSensor rightSensor = new DistanceSensor(5);//TODO: add IDs
+  DriveSubsystem driveSubsystem;
 
-    @Override
+  public LevitateCommand(DriveSubsystem driveSubsystem) {
+    this.driveSubsystem = driveSubsystem;
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
     public void execute() {
       if (!leftSensor.isOff() && !rightSensor.isOff()){
         driveSubsystem.drive(0, 0.1, 0, false);
-      }
+      }//drive right until both sensors are out of the platform
       if (leftSensor.isOff()){
         driveSubsystem.driveAxis(0, 0, 0.1, false, new Translation2d(-10.375,12.375));
-      }
+      }//if left sensor is out of the platform, pivot on left wheel
       if (rightSensor.isOff()){
         driveSubsystem.driveAxis(0, 0, 0.1, false, new Translation2d(-10.375,-12.375));
-      }
+      }//if right sensor is out of the platform, pivot on right wheel
       if (leftSensor.isOff() && rightSensor.isOff()){
-        //drive right and foot
+        //TODO: PUT DOWN FOOT
+        if (levitateSwitch.get()) {
+          driveSubsystem.drive(0, 0.0, 0, false);
+          driveSubsystem.xWheels();
+        } else {
+          driveSubsystem.drive(0, 0.1, 0, false);
+        }//if limit switch is triggered, stop all motors
       }
-      
     }
 
-    @Override
-    public void end(boolean interrupted) {}
+  @Override
+  public void end(boolean interrupted) {
+  }
 
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
-    // while (!leftSensor.isOff() && !rightSensor.isOff()){ 
-    //   //driveRightSlowly();
-    // }
-
-    // if (leftSensor.isOff()){
-    //   while(!rightSensor.isOff()){
-    //     //pivotOnLeftWheel();
-    //   }
-    // } 
-    // if (rightSensor.isOff()){
-    //   while(!leftSensor.isOff()){
-    //     //pivotOnRightWheel();
-    //   }
-    // }
-        //rampDriveOff();//deployes rubber foot and drives off in one function    
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
