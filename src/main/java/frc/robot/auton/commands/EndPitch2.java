84b039b1faa7f6d74ddf3d437f951ed73eb42f02
Nitsuj06 <nitsujwriter@gmail.com>
2023-02-27 12:52:54 -0500

    completed autons (untested)

---
+++ src/main/java/frc/robot/auton/commands/EndPitch2.java
@@ -0,0 +1,46 @@
+package frc.robot.auton.commands;
+
+import java.util.Currency;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.sensors.Gyro;
+import frc.robot.subsystems.drive.DriveSubsystem;
+
+public class EndPitch2 extends CommandBase {
+    private final DriveSubsystem driveSubsystem;
+    double initTime;
+    boolean setTime = true;
+    private Gyro gyro;
+    boolean two = false;
+    double initTime2;
+    public EndPitch2(DriveSubsystem swerve, Gyro gyro) {
+        this.gyro = gyro;
+        driveSubsystem = swerve;
+    
+      }
+      @Override
+      public void initialize() {
+      }
+      @Override
+      public void execute(){
+        if (Math.abs(gyro.getGyroPitch()) >= 13 && setTime){
+          initTime = System.currentTimeMillis();
+          setTime = false;
+        }
+        if (!setTime && System.currentTimeMillis() - initTime > 2){
+          if (!two && Math.abs(gyro.getGyroPitch()) >= 13){
+            two = true;
+            initTime2 = System.currentTimeMillis();
+          }
+        }
+        // System.out.println("Pitch: " + gyro.getGyroPitch());
+      }
+      @Override
+      public void end(boolean interrupted){
+        System.out.println("Balance!");
+      }
+      @Override
+      public boolean isFinished() {
+        return two && System.currentTimeMillis() - initTime2 >= 500;
+      }
+}
--- src/main/java/frc/robot/auton/paths/PlaceCharge.java
+++ src/main/java/frc/robot/auton/paths/PlaceCharge.java
@@ -17,8 +17,10 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.auton.commands.Balance;
 import frc.robot.auton.commands.EndPitch;
+import frc.robot.auton.commands.EndPitch2;
 import frc.robot.sensors.Gyro;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.ArmSubsystem.Preset;
@@ -27,14 +29,14 @@ import frc.robot.subsystems.drive.commands.ResetOdometryCommand;
 import frc.robot.subsystems.grabber.GrabberSubsystem;
 import frc.robot.subsystems.grabber.commands.SetGrabCommand;
 
-public class PlaceOut {
+public class PlaceCharge {
   public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(Math.PI, Math.PI);
   public static final double x = Units.inchesToMeters(10.375); // 10.375"
   public static final double y = Units.inchesToMeters(12.375); // 12.375"
   public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(new Translation2d(y, x),new Translation2d(y, -x), new Translation2d(-y, x), new Translation2d(-y, -x));
 
   
-  PathPlannerTrajectory placePath = PathPlanner.loadPath("place - out", new PathConstraints(2, 2));
+  PathPlannerTrajectory placePath = PathPlanner.loadPath("place - out - charge", new PathConstraints(2, 2));
   PathPlannerState placeState = new PathPlannerState();
   /** Example static factory for an autonomous command. */
   public CommandBase loadAuto(Gyro gyro, DriveSubsystem swerve, ArmSubsystem armSubsystem, GrabberSubsystem grabberSubsystem) { 
@@ -62,7 +64,9 @@ public class PlaceOut {
         swerve::getPose, // Functional interface to feed supplier
         kDriveKinematics, new PIDController(0.3, 0.0, 0.0), new PIDController(0.3, 0.0, 0.0), new PIDController(0.5, 0, 0),
         swerve::setModuleStates, true, swerve);
-    ParallelCommandGroup group = new ParallelCommandGroup(safe, placePathController);
-    return Commands.sequence(resetOdo, pickup, grab, place, unGrab, group);// 
+    
+    EndPitch2 end = new EndPitch2(swerve, gyro);
+    ParallelDeadlineGroup group = new ParallelDeadlineGroup(end, safe, placePathController, end);
+    return Commands.sequence(resetOdo, pickup, grab, place, unGrab, group);
   }
 }
