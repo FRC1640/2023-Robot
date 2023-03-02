0a619f8a7b49b58b175777f620c95fa0a0f47a6d
renieharris <renieharris124@gmail.com>
2023-03-01 20:54:08 -0500

    limit switch?

--- src/main/java/frc/robot/subsystems/foot/FootSubsystem.java
+++ src/main/java/frc/robot/subsystems/foot/FootSubsystem.java
@@ -5,16 +5,20 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
+
 
 public class FootSubsystem extends SubsystemBase{
    DoubleSolenoid footSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+   DigitalInput footLimitSwitch = new DigitalInput(9999); // uh unknown channel
 
     public FootSubsystem(){
         footSolenoid.set(Value.kForward);
     }
 
     public void toggleClamped() {
-        
-        footSolenoid.toggle();
+        if (! footLimitSwitch.get()){ // if limit switch not tripped
+            footSolenoid.toggle();
+        }
     }
 }
